98ea91ccf9b3ff01274e2fdeba90c4cd647f6388
Masih,Ashish <masih.ashish@gmail.com>
2019-08-08 18:02:57 -0600

    Cleanup of extra files

--- ET_Calculator_streamlined_v11_GIT/create_table_command.sql
+++
@@ -1,5 +0,0 @@
-﻿-- Script Date: 6/14/2019 8:47 AM  - ErikEJ.SqlCeScripting version 3.5.2.81
-CREATE TABLE [RefETrsz_Results] (
-  [SNo] INTEGER NOT NULL
-, CONSTRAINT [PK_RefETrsz_Results] PRIMARY KEY ([SNo])
-);
